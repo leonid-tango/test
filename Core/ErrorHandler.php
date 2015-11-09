6cb5249a1edbc04299cdde0cc59ea8aa9a3344bf
Leonid <leonidkoida@gmail.com>
2015-11-09 08:44:53 +0200

    Created class for exceptions handling

---
+++ Core/ErrorHandler.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leonid
+ * Date: 11/9/15
+ * Time: 8:39 AM
+ */
+
+namespace Core;
+
+
+class ExceptionHandler
+{
+
+    public function notFound()
+    {
+
+    }
+
+    public function notEnoughData()
+    {
+
+    }
+
+
+    public function notEqual()
+    {
+
+    }
+
+    public function notEnoughChars()
+    {
+
+    }
+
+}
